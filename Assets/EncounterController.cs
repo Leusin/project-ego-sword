f2208aa8bb264ee8ca2586c84e3f6de1c1193f01
Leusin <98079486+Leusin@users.noreply.github.com>
2024-10-27 11:11:21 +0900

    add: bind input keys

--- Assets/EncounterController.cs
+++ Assets/EncounterController.cs
@@ -2,20 +2,25 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
 
 public class EncounterController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
-    public float rotateSpeed = 1f;
     public Transform WeaponMountPosition;
 
-    //[Border]
-    private PlayerInputController playerInputCtrl;
+    // -----
+
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float rotateSpeed = 1f;
 
     private bool m_isRotating;
     private bool m_isMoving;
+    private float m_direction;
 
-    private NavMeshAgent m_NavMeshAgent;
+    private NavMeshAgent m_navMeshAgent;
+
+    private PlayerInputController m_playerInputCtrl;
 
     // -----
 
@@ -24,12 +29,12 @@ public class EncounterController : MonoBehaviour
         playerController.transform.SetParent(WeaponMountPosition.transform);
         playerController.transform.localPosition = Vector3.zero; ;
         playerController.transform.localRotation = Quaternion.identity;
-        m_NavMeshAgent.enabled = false;
+        m_navMeshAgent.enabled = false;
     }
 
     public void Unequip()
     {
-        m_NavMeshAgent.enabled = true;
+        m_navMeshAgent.enabled = true;
         transform.SetParent(null);
         this.enabled = false;
     }
@@ -38,39 +43,34 @@ public class EncounterController : MonoBehaviour
 
     private void Awake()
     {
-        m_NavMeshAgent = GetComponent<NavMeshAgent>();
-        playerInputCtrl = PlayerInputController.Instance;
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+
+        this.enabled = false;
     }
 
-    private void Start()
-    {
-        this.enabled = false ;
-    }
-
-    void Update()
-    {
-        float xInput = playerInputCtrl.MoveInput.x;
-        float yInput = playerInputCtrl.MoveInput.y;
-
-        Move(xInput);
-    }
 
     private void OnEnable()
     {
-        playerInputCtrl.Crounch.AddListener(Unequip);
+        m_playerInputCtrl = PlayerInputController.Instance;
+
+        if (m_playerInputCtrl)
+        {
+            m_playerInputCtrl.Move += Move;
+        }
     }
 
-    private void OnDisable() 
+    private void OnDisable()
     {
-        playerInputCtrl.Crounch.RemoveListener(Unequip);
+        if (m_playerInputCtrl)
+        {
+            m_playerInputCtrl.Move -= Move;
+        }
     }
 
-    // -----
-
-    private void Move(float direction)
+    private void Update()
     {
         // 이동 방향
-        var move = new Vector3(direction, 0f, 0f);
+        Vector3 move = new Vector3(m_direction, 0f, 0f);
 
         m_isMoving = move.sqrMagnitude > 0f;
 
@@ -86,7 +86,13 @@ public class EncounterController : MonoBehaviour
             var scaledMoveSpeed = moveSpeed * Time.deltaTime;
             transform.position += move * scaledMoveSpeed;
         }
+    }
 
+    // -----
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        m_direction = context.ReadValue<Vector2>().x;
     }
 
     private IEnumerator RotateTowards(Vector3 targetDirection)
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,36 +12,95 @@ public class PlayerController : MonoBehaviour
 
     [Header("AttractRange")]
     private float attractRange;
-    public float maxAttractRange = 10f;
-    public float attractAcceleration = 10f;
+    public float maxAttaractRange = 10f;
+    public float attaractAcceleration = 10f;
+    public float attaractDeceleration = 8f;
 
-    private LayerMask m_layerEncounter;
+    private bool m_isAttaract;
 
     private Rigidbody m_rigidbody;
+    private LayerMask m_layerEncounter;
+
     private PlayerInput m_playerinput;
     private PlayerInputController m_playerInputCtrl;
 
     // -----
 
-
-    // -----
-
     private void Awake()
     {
-        m_playerinput = GetComponent<PlayerInput>();
         m_rigidbody = GetComponent<Rigidbody>();
 
         m_layerEncounter = LayerMask.GetMask("Encounter");
+
+        m_playerinput = GetComponent<PlayerInput>();
+        m_playerInputCtrl = GetComponent<PlayerInputController>();
     }
 
-    private void Start()
+
+    private void OnGUI()
     {
-        m_playerInputCtrl = PlayerInputController.Instance;
+        if (m_playerinput != null && m_playerinput.currentActionMap != null)
+        {
+            string actionMapName = "Current Action Map: " + m_playerinput.currentActionMap.name;
+            string controlSchemeName = "Current Control Scheme: " + m_playerinput.currentControlScheme;
+
+            // 라벨의 크기 계산
+            Vector2 actionMapSize = GUI.skin.label.CalcSize(new GUIContent(actionMapName));
+            Vector2 controlSchemeSize = GUI.skin.label.CalcSize(new GUIContent(controlSchemeName));
+
+            // 화면의 우상단에 위치 설정
+            float x = Screen.width - actionMapSize.x - 200; // 오른쪽 여백 10 픽셀
+            float y = 10; // 위쪽 여백 10 픽셀
+
+            // 라벨을 화면에 표시
+            GUI.Label(new Rect(x, y, actionMapSize.x, actionMapSize.y), actionMapName);
+            GUI.Label(new Rect(x, y + actionMapSize.y + 5, controlSchemeSize.x, controlSchemeSize.y), controlSchemeName);
+        }
+        else
+        {
+            GUI.Label(new Rect(10, 10, 300, 20), "PlayerInput or Action Map is not set.");
+        }
+    }
+
+    private void OnEnable()
+    {
+        m_playerInputCtrl.Attaract += Attaract;
+    }
+
+    public void OnDisable()
+    {
+        m_playerInputCtrl.Attaract -= Attaract;
     }
 
     private void Update()
     {
-        Attaract();
+        if(m_isAttaract)
+        {
+            if (attractRange < maxAttaractRange)
+            {
+                attractRange += Time.deltaTime * attaractAcceleration;
+            }
+            else
+            {
+                attractRange = maxAttaractRange;
+            }
+
+            // Temp
+            UnityEngine.Collider[] hitColliders = Physics.OverlapSphere(transform.position, attractRange);
+            foreach (var hitCollider in hitColliders)
+            {
+                var navMeshAgent = hitCollider.GetComponent<NavMeshAgent>();
+                if (navMeshAgent != null)
+                {
+                    navMeshAgent.SetDestination(transform.position);
+                }
+            }
+        }
+        else
+        {
+            // 매 프레임마다 attractRange를 감소시킴
+            attractRange = Mathf.Max(0f, attractRange - Time.deltaTime * attaractDeceleration);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -74,37 +133,15 @@ public class PlayerController : MonoBehaviour
 
     // -----
 
-    private void Attaract()
+    private void Attaract(InputAction.CallbackContext context)
     {
-        if (m_playerInputCtrl.IsAttract)
+        if (context.performed)
         {
-            // 매 프레임마다 attractRange를 증가시킴
-            if (attractRange < maxAttractRange)
-            {
-                attractRange += Time.deltaTime * attractAcceleration;
-            }
-            else
-            {
-                attractRange = maxAttractRange;
-            }
-
-            //TEST
-            // Attract Encounter
-            UnityEngine.Collider[] hitColliders = Physics.OverlapSphere(transform.position, attractRange);
-            foreach (var hitCollider in hitColliders)
-            {
-                var navMeshAgent = hitCollider.GetComponent<NavMeshAgent>();
-                if (navMeshAgent != null)
-                {
-                    navMeshAgent.SetDestination(transform.position);
-                }
-            }
-
+            m_isAttaract = true;
         }
-        else
+        else if (context.canceled)
         {
-            // 매 프레임마다 attractRange를 감소시킴
-            attractRange = Mathf.Max(0f, attractRange - Time.deltaTime * attractAcceleration);
+            m_isAttaract = false;
         }
     }
 }
