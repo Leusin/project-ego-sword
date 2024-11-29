1dbac758e4acf0f6ac9395ac1ca0380454d29361
Leusin <98079486+Leusin@users.noreply.github.com>
2024-11-29 15:38:22 +0900

    Set Physics Layer

--- Assets/Scripts/CharacterControll/AbilitySystem/StateScripts/CheckMovement.cs
+++ Assets/Scripts/CharacterControll/AbilitySystem/StateScripts/CheckMovement.cs
@@ -6,11 +6,6 @@ namespace ProjectEgoSword
     [CreateAssetMenu(fileName = "New State", menuName = "ProjectEgoSword/AbilityData/CheckMove")]
     public class CheckMovement : StateData<CharacterControl>
     {
-        // 캐릭터 이동 입력이 버튼이 아닌 조이스틱이라서
-        // (부드러운 이동을 위해) 이동 취소의 딜레이 타이머
-        private float _timer;
-        private float _moveCancelDelay = -0.1f;
-
         public override void OnEnter(CharacterControl monobehaviour, Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
         }
@@ -20,18 +15,10 @@ namespace ProjectEgoSword
             if (monobehaviour.moveLeft || monobehaviour.moveRight)
             {
                 animator.SetBool(CharacterControl.TransitionParameter.Move.ToString(), true);
-                _timer = 0f;
-                Debug.Log("Chehck Movement Timer Init");
             }
-            else //if (_timer > _moveCancelDelay)
+            else
             {
                 animator.SetBool(CharacterControl.TransitionParameter.Move.ToString(), false);
-                _timer = 0f;
-                Debug.Log("Chehck Movement Timer Update Stop");
-            }
-            //else
-            {
-                _timer += Time.deltaTime;
             }
         }
 
---
+++ Assets/Scripts/Layer Adder/LayerAdder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    public enum RB_Layers
+    {
+        PostProcessing,
+        Character,
+        Bot,
+        Ground,
+        Ragdoll,
+    }
+
+    public class LayerAdder : MonoBehaviour
+    {
+
+    }
+}
