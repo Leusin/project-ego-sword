462bb9dda179c83c5f449e78ef4c8ed8b749a4cd
Leusin <98079486+Leusin@users.noreply.github.com>
2024-11-29 15:05:17 +0900

    Fix RunToTurn

--- Assets/Scripts/CharacterControll/AbilitySystem/StateScripts/CheckMovement.cs
+++ Assets/Scripts/CharacterControll/AbilitySystem/StateScripts/CheckMovement.cs
@@ -6,11 +6,13 @@ namespace ProjectEgoSword
     [CreateAssetMenu(fileName = "New State", menuName = "ProjectEgoSword/AbilityData/CheckMove")]
     public class CheckMovement : StateData<CharacterControl>
     {
-        float timer;
+        // 캐릭터 이동 입력이 버튼이 아닌 조이스틱이라서
+        // (부드러운 이동을 위해) 이동 취소의 딜레이 타이머
+        private float _timer;
+        private float _moveCancelDelay = -0.1f;
 
         public override void OnEnter(CharacterControl monobehaviour, Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            timer = 0f;
         }
 
         public override void UpdateAbility(CharacterControl monobehaviour, Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -18,17 +20,19 @@ namespace ProjectEgoSword
             if (monobehaviour.moveLeft || monobehaviour.moveRight)
             {
                 animator.SetBool(CharacterControl.TransitionParameter.Move.ToString(), true);
-                timer = 0f;
+                _timer = 0f;
+                Debug.Log("Chehck Movement Timer Init");
             }
-            else if(timer > 1f)
+            else //if (_timer > _moveCancelDelay)
             {
                 animator.SetBool(CharacterControl.TransitionParameter.Move.ToString(), false);
+                _timer = 0f;
+                Debug.Log("Chehck Movement Timer Update Stop");
             }
-            else
+            //else
             {
-                timer++;
+                _timer += Time.deltaTime;
             }
-
         }
 
         public override void OnExit(CharacterControl monobehaviour, Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
